92bc3da22af115fbc923c8743418e12255e02692
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-06-04 20:40:03 -0300

    Update Auth

--- auth.ts
+++ auth.ts
@@ -5,6 +5,17 @@ import Google from "next-auth/providers/google";
 
 const config = {
   providers: [Google],
+  callbacks: {
+    authorized({ request, auth }) {
+      const { pathname } = request.nextUrl;
+
+      if (pathname === "/middleware") {
+        return !!auth;
+      }
+
+      return true;
+    },
+  },
 } satisfies NextAuthConfig;
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
